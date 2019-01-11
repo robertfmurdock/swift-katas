9c80572fcd125c8b9c1c3834f63a3255bdc3a072
Robert Murdock <yzerick@gmail.com>
2019-01-11 13:42:13 -0500

    Initial Commit

---
+++ kata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kata
+//
+//  Created by Rob Murdock on 1/11/19.
+//  Copyright © 2019 Rob Murdock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
